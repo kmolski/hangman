8715cb56c309aa52853355ffb7e144d290eb1dad
kmolski <starfighter413@gmail.com>
2021-07-31 20:31:41 +0200

    Add package-info.java files

---
+++ src/main/java/pl/kmolski/hangman/repo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Repository implementations for the hangman game.
+ *
+ * @since 1.1
+ * @author Krzysztof Molski
+ * @version 1.1
+ */
+package pl.kmolski.hangman.repo;
---
+++ src/main/java/pl/kmolski/hangman/service/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Service implementations for the hangman game.
+ *
+ * @since 1.1
+ * @author Krzysztof Molski
+ * @version 1.1
+ */
+package pl.kmolski.hangman.service;
